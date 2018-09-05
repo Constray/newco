fa900b5206d15d90a5cf7f2e17c36e78296fc4e8
Denis Kozlov <ConstrayRUS@gmail.com>
2018-09-05 09:37:25 +0400

    Task 5 completed

---
+++ src/main/java/com/andreitop/newco/common/DtoApiErrorConstant.java
@@ -0,0 +1,9 @@
+package com.andreitop.newco.common;
+
+public class DtoApiErrorConstant {
+    public static final String ACCESS_DENIED = "Error: access denied";
+    public static final String NULL_POINTER = "Error: NPE";
+    public static final String CREATED_ERROR = "The entity was not created";
+    public static final String UPDATED_ERROR = "The entity was not updated";
+    public static final String DELETED_ERROR = "The entity was not deleted";
+}
