0966c425a8171891830a729802b98c624725451b
Denis Kozlov <ConstrayRUS@gmail.com>
2018-09-09 17:33:43 +0400

    Task completed

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -4,8 +4,7 @@ package com.andreitop.newco.aspect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -20,6 +19,63 @@ public class LoggingAspect {
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @Before("com.andreitop.newco.aspect.PointcutContainer.serviceDelete()")
+    public void beforeServiceDelete(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @Before("com.andreitop.newco.aspect.PointcutContainer.serviceSave()")
+    public void beforeServiceSave(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+
+    @Before("com.andreitop.newco.aspect.PointcutContainer.serviceUpdate()")
+    public void beforeServiceUpdate(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
+    }
+    @After("com.andreitop.newco.aspect.PointcutContainer.repositoryFind()")
+    public void afterRepoFind(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " was just called");
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.serviceDelete()")
+    public void afterServiceDelete(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " was just called");
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.serviceSave()")
+    public void afterServiceSave(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " was just called");
+    }
+
+    @After("com.andreitop.newco.aspect.PointcutContainer.serviceUpdate()")
+    public void afterServiceUpdate(JoinPoint joinPoint) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " was just called");
+    }
+
+    @AfterReturning(pointcut = "com.andreitop.newco.aspect.PointcutContainer.serviceFindAll())",
+                    returning = "result")
+    public void afterServiceFindAll(JoinPoint joinPoint, Object result) {
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " was just called");
+        logger.info(" ---> Method return value: " + result.toString() );
     }
 
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -4,7 +4,23 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class PointcutContainer {
 
-    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
+    @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*(*))")
     public void repositoryFind() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.service.*Service*.*save*(*))")
+    public void serviceSave() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.service.*Service*.*delete*(*))")
+    public void serviceDelete() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.service.*Service*.*update*(*))")
+    public void serviceUpdate() {
+    }
+
+    @Pointcut("execution( * com.andreitop.newco.service.*Service*.findAll*())")
+    public void serviceFindAll() {
     }
 }
